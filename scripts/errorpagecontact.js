150da2f90f623e61c692724d958646acc6f5cd33
Aayush Swodari <66193594+Aayushdn@users.noreply.github.com>
2025-12-02 21:30:23 +0000

    small change to check

--- scripts/errorpagecontact.js
+++ scripts/errorpagecontact.js
@@ -1,53 +1,55 @@
-const submitButton = document.querySelector("#submitButton")
+const submitButton = document.querySelector("#submitButton");
 const fname = document.querySelector("#fname");
 const email = document.querySelector("#email");
-const errorText = document.querySelector(".errorText")
+const errorText = document.querySelector(".errorText");
 const phone = document.querySelector("#phone");
 const subject = document.querySelector("#subject");
 const message = document.querySelector("#message");
-errorText.textContent=""
+errorText.textContent = "";
 
-console.log(errorText)
+console.log(errorText);
 
-submitButton.addEventListener("click",function(e)
-    {
-        e.preventDefault();
-        const phoneValue = phone.value.trim();
-        const emailValue = email.value
-        const fnameValue = fname.value
-        const subjectValue = subject.value
-        const messageValue = message.value
+function displayErrorAsList(errorArray) {
+  errorText.textContent = "";
+  errorArray.forEach((element) => {
+    const li = document.createElement("li");
+    li.textContent = element;
+    errorText.appendChild(li);
+  });
+}
 
-        let errors = [];
-        if (messageValue === "") {
-            errors.push("Please enter your message.");
-            
-        }
-        if (subjectValue === "") {
-            errors.push("Please enter your subject.");
-            
-        }
+submitButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const phoneValue = phone.value.trim();
+  const emailValue = email.value;
+  const fnameValue = fname.value;
+  const subjectValue = subject.value;
+  const messageValue = message.value;
 
-         if (fnameValue === "") {
-            errors.push("Please enter your name.");
-            
-         }
-        if (phoneValue === "") {
-            errors.push("Please enter your phone number.");
-            
-        }
-        console.log(emailValue.match((/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)))
-         if(emailValue.match((/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)) == null){
-            errors.push("Please enter your e-mail.");
-      }
-      if (errors.length > 0) {
-         errorText.textContent = errors.join(" ");
-      } 
-      else 
-      {
-        errorText.style.color = "green";
-        errorText.textContent = "Your message has been sent!";
-        document.querySelector(".contactForm").reset(); 
-      }
-})
+  let errors = [];
+  if (messageValue === "") {
+    errors.push("Please enter your message.");
+  }
+  if (subjectValue === "") {
+    errors.push("Please enter your subject.");
+  }
 
+  if (fnameValue === "") {
+    errors.push("Please enter your name.");
+  }
+  if (phoneValue === "") {
+    errors.push("Please enter your phone number.");
+  }
+  console.log(emailValue.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g));
+  if (emailValue.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g) == null) {
+    errors.push("Please enter your e-mail.");
+  }
+  if (errors.length > 0) {
+    //  errorText.textContent = errors.join("&nbsp");
+    displayErrorAsList(errors);
+  } else {
+    errorText.style.color = "green";
+    errorText.textContent = "Your message has been sent!";
+    document.querySelector(".contactForm").reset();
+  }
+});
